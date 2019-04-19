6b31595140753968dbd3cc51505326d0b2012a13
Muhammad Umar Iqbal <umarzapak5@hotmail.com>
2019-04-19 15:16:36 -0500

    frontend controller files

--- src/main/java/Controller/AuthController/UserLogin.java
+++ src/main/java/Controller/AuthController/UserLogin.java
@@ -39,6 +39,6 @@ public class UserLogin extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
--- src/main/webapp/controller/users.js
+++ src/main/webapp/controller/users.js
@@ -1,1 +1,4 @@
 // Ajax Calls for Users
+
+
+//Ajax calls for Team
