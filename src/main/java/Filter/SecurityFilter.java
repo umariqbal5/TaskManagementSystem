dddbf4d2e868abbc1ea91335be49674e334d6c4e
Viet Anh Phan <phanvietanh90@gmail.com>
2019-04-21 19:22:56 -0500

    add filter user.jsp

--- src/main/java/Filter/SecurityFilter.java
+++ src/main/java/Filter/SecurityFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = { "/tasks", "/home.jsp"})
+@WebFilter(urlPatterns = { "/tasks", "/home.jsp","/users.jsp"})
 public class SecurityFilter implements Filter {
 
     public SecurityFilter() {
